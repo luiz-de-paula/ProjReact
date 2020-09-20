d2d3f56aab9f38794e4ce6b8e98bd69d268fc5ea
Luiz-de-Paula <eletronica.do.lui@gmai.com>
2020-09-20 15:08:07 -0300

    primeiro comit do projReact

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './app.css';
+import profile from './assets/profile.jpg';
+
+function App (){
+  return <img width ="200" src={profile}/>
+}
+
+export default App;
